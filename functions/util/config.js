29bab595155e39f9b5dad5ab253cfa8d97da5dc2
Ismail Opatola <opatolamails@gmail.com>
2019-06-09 09:39:36 +0100

    add firebase Config file

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyDCPf6kB83SGEZ0Os2oiF1vSPUSD13z1D0",
+  authDomain: "would-you-rather-app-c5895.firebaseapp.com",
+  databaseURL: "https://would-you-rather-app-c5895.firebaseio.com",
+  projectId: "would-you-rather-app-c5895",
+  storageBucket: "would-you-rather-app-c5895.appspot.com",
+  messagingSenderId: "163168259969",
+  appId: "1:163168259969:web:7f3418089669a402"
+};
