bb72584b7c17004ddbdf02ca59ce3a429627ba7e
Ismail Opatola <opatolamails@gmail.com>
2019-06-09 11:03:11 +0100

    initialize express

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,21 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions"),
+  app = require("express")(),
+  cors = require("cors");
+
+app.use(cors({ origin: true }));
+
+const { signup, login } = require("./handlers/users");
+
+// users routes
+app.post('/signup', signup)
+app.post('/login', login)
+
+
+exports.api = functions.https.onRequest(app);
+
+// app.get("/", (request, response) => {
+//   response.send("Hello from Firebase!");
+// });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
