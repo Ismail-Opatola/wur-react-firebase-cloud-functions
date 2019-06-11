fe70b049bbd824b3972d22de9bdefcc958e80817
Ismail Opatola <opatolamails@gmail.com>
2019-06-11 03:58:01 +0100

    add signup and login routes

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,17 @@
-const functions = require("firebase-functions"),
-  app = require("express")(),
-  cors = require("cors");
+const functions = require('firebase-functions');
+const app = require("express")();
 
-app.use(cors({ origin: true }));
+const cors = require("cors");
+app.use(cors({origin: true}));
 
 const { signup, login } = require("./handlers/users");
 
 // users routes
-app.post('/signup', signup)
-app.post('/login', login)
+app.post("/signup", signup);
+app.post("/login", login);
 
+app.get("/", (request, response) => {
+  response.send("Hello from Firebase!");
+});
 
 exports.api = functions.https.onRequest(app);
-
-// app.get("/", (request, response) => {
-//   response.send("Hello from Firebase!");
-// });
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
