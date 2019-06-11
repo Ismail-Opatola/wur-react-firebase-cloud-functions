7e610a0dd1bcffa5a98756998eedb7a2cce1afbc
Ismail Opatola <opatolamails@gmail.com>
2019-06-11 11:07:03 +0100

    add login validation handler

--- functions/util/validation.js
+++ functions/util/validation.js
@@ -1,32 +1,51 @@
-const isEmail = (email) => {
-    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    
-    if(email.match(regEx)) return true;
-    else return false;
+const EMPTY_DATA = "Must not be empty";
+const INVALID_EMAIL_ADDRESS = "Must be a valid email address";
+const INVALID_PASSWORD = "Must not be less than 6 characters";
+
+const isEmail = email => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (email.match(regEx)) return true;
+  else return false;
 };
 
-const isEmpty = (string) => {
-    if (string.trim() === '') return true;
-    else return false;
+const isEmpty = string => {
+  if (string.trim() === "") return true;
+  else return false;
 };
 
-exports.validateSignupData = (data) => {
-    let errors = {};
+exports.validateSignupData = data => {
+  let errors = {};
 
-    if (isEmpty(data.email)) {
-        errors.email = 'Must not be empty';
-    } else if (!isEmail(data.email)) {
-        errors.email = 'Must be a valid email address';
-    }
+  if (isEmpty(data.email)) {
+    errors.email = EMPTY_DATA;
+  } else if (!isEmail(data.email)) {
+    errors.email = INVALID_EMAIL_ADDRESS;
+  }
 
-    if(isEmpty(data.password)) errors.password = 'Must not be empty';
-    if(data.password.length < 6) errors.password = 'Must not be less than 6 characters';
-    if(isEmpty(data.firstName)) errors.firstName = 'Must not be empty';
-    if(isEmpty(data.lastName)) errors.lastName = 'Must not be empty';
+  if (isEmpty(data.password)) errors.password = EMPTY_DATA;
+  if (data.password.length < 6) errors.password = INVALID_PASSWORD;
+  if (isEmpty(data.firstName)) errors.firstName = EMPTY_DATA;
+  if (isEmpty(data.lastName)) errors.lastName = EMPTY_DATA;
 
-    return {
-        errors,
-        valid: Object.keys(errors).length === 0 ? true : false
-    };
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0 ? true : false
+  };
 };
 
+exports.validateLoginData = data => {
+  let errors = {};
+
+  if (isEmpty(data.email)) {
+    errors.email = EMPTY_DATA;
+  } else if (!isEmail(data.email)) {
+    errors.email = INVALID_EMAIL_ADDRESS;
+  }
+  if (isEmpty(data.password)) errors.password = EMPTY_DATA;
+
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0 ? true : false
+  };
+};
