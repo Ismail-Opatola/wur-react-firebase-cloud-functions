bf206430de34d3e0e30d6b4bc9abbc629aa27b0b
Ismail Opatola <opatolamails@gmail.com>
2019-06-13 18:03:43 +0100

    create authentication middleware
    Authomaticaly checks protected routes for user's token

---
+++ functions/util/fbAuth.js
@@ -0,0 +1,36 @@
+const { admin, db } = require("./admin");
+
+module.exports = (req, res, next) => {
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ") &&
+    req.headers.authorization.split("Bearer ")[1].trim()
+  ) {
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  } else {
+    console.error("No token found");
+    return res.status(403).json({ error: "Unauthorized" });
+  }
+
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(decodedToken => {
+      req.user = decodedToken;
+      return db
+        .collection("users")
+        .where("userId", "==", req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then(data => {
+      req.user.fullName = data.docs[0].data().fullname;
+      req.user.imageUrl = data.docs[0].data().imageUrl;
+      return next();
+    })
+    .catch(err => {
+      console.log("Error while verifying token", err);
+      return res.status(403).json({error: "Unauthorized"});
+    });
+};
