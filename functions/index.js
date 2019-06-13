670ce7154e44f2ec807df0672033c822722cd582
Ismail Opatola <opatolamails@gmail.com>
2019-06-13 12:36:47 +0100

    add /questions route

--- functions/index.js
+++ functions/index.js
@@ -5,10 +5,14 @@ const cors = require("cors");
 app.use(cors({origin: true}));
 
 const { signup, login } = require("./handlers/users");
+const { getAllQuestions } = require("./handlers/questions");
 
 // users routes
 app.post("/signup", signup);
 app.post("/login", login);
+
+// question routes
+app.get('/questions', getAllQuestions)
 
 app.get("/", (request, response) => {
   response.send("Hello from Firebase!");
