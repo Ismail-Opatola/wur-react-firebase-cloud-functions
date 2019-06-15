5dd58b9b54aa59ac6b378352e1360c73b6675330
Ismail Opatola <opatolamails@gmail.com>
2019-06-16 00:14:39 +0100

    add get_single_question_route

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 app.use(cors({origin: true}));
 
 const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser, getUserDetails } = require("./handlers/users");
-const { getAllQuestions, postQuestion } = require("./handlers/questions");
+const { getAllQuestions, postQuestion, getQuestion } = require("./handlers/questions");
 
 // users routes
 app.post("/signup", signup);
@@ -14,11 +14,12 @@ app.post("/login", login);
 app.post('/user/image', FBAuth, uploadImage);
 app.post('/user', FBAuth, addUserDetails);
 app.get('/user', FBAuth, getAuthenticatedUser);
-app.get('/user/:id', getUserDetails);
+app.get('/user/:userId', getUserDetails);
 
 // question routes
 app.get('/questions', getAllQuestions);
-app.post('/question', FBAuth, postQuestion)
+app.post('/question', FBAuth, postQuestion);
+app.get('/question/:questionId', getQuestion);
 
 app.get("/", (request, response) => {
   response.send("Hello from Firebase!");
