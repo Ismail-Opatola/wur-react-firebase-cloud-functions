cd338d49c4fece5ea8ef26050b56a8925b85950d
Ismail Opatola <opatolamails@gmail.com>
2019-06-17 21:01:11 +0100

    trigger cloud-func - create_notification_on_vote

--- functions/index.js
+++ functions/index.js
@@ -10,7 +10,8 @@ const {
   uploadImage,
   addUserDetails,
   getAuthenticatedUser,
-  getUserDetails
+  getUserDetails,
+  markNotificationsRead
 } = require("./handlers/users");
 const {
   getAllQuestions,
@@ -28,6 +29,7 @@ app.post("/user/image", FBAuth, uploadImage);
 app.post("/user", FBAuth, addUserDetails);
 app.get("/user", FBAuth, getAuthenticatedUser);
 app.get("/user/:userId", getUserDetails);
+app.post("/notifications", FBAuth, markNotificationsRead);
 
 // question routes
 app.post("/question", FBAuth, postQuestion);
@@ -44,6 +46,56 @@ app.get("/", (request, response) => {
 });
 
 exports.api = functions.https.onRequest(app);
+
+// @ FUNCTIONS >> CLOUD FIRESTORE EVENT TRIGGERS
+// @ ref: https://firebase.google.com/docs/functions/firestore-events
+//        https://cloud.google.com/functions/docs/bestpractices/tips#performance
+//        https://www.youtube.com/playlist?list=PLIivdWyY5sqK5zce0-fd1Vam7oPY-s_8X
+
+// TODO: onImageChange
+
+exports.createNotificationOnVote = functions
+  .region("us-central1")
+  .firestore.document("/questions/{questionId}/")
+  .onUpdate((snapshot, context) => {
+    try {
+      // @ Retrieve the current and previous value
+      const { questionId } = context.params;
+      const data = snapshot.after.data();
+      const previousData = snapshot.before.data();
+
+      // @ We'll only update if the name has changed.
+      // @ This is crucial to prevent infinite loops.
+      if (
+        data.optionOne.votes.length == previousData.optionOne.votes.length &&
+        data.optionTwo.votes.length == previousData.optionTwo.votes.length
+      )
+        return null;
+
+      // ===========================================================
+      // @ Logic as cloud function triggers with event queues
+      // ===========================================================
+
+      if (
+        data.optionOne.votes.includes(context.auth.uid) ||
+        data.optionTwo.votes.includes(context.auth.uid)
+      )
+        return db
+          .collection(`notifications`)
+          .doc()
+          .set({
+            createdAt: new Date().toISOString(),
+            recipient: snapshot.data().authorId,
+            sender: context.auth.uid,
+            type: "vote",
+            read: false,
+            questionId: questionId
+          })
+          .catch(err => console.error(err));
+    } catch (err) {
+      console.error(err);
+    }
+  });
 
 // @ wipe questionID from author and voter's doc
 exports.onQuestionDelete = functions
@@ -71,7 +123,120 @@ exports.onQuestionDelete = functions
           questions: admin.firestore.FieldValue.arrayRemove(questionId),
           score: admin.firestore.FieldValue.increment(-1)
         });
+        // TODO: deleteNotificationOnQuestionDelete
         return batch.commit();
       })
       .catch(err => console.error(err));
   });
+
+// UNMARKED
+
+// Global variable
+// function hasChange(votes, previousVotes) {
+//   let change = votes.filter(vote => !previousVotes.includes(vote));
+//   return change;
+// }
+
+/**
+ * exports.createNotificationOnVote = functions
+  .region("us-central1")
+  .firestore.document("/questions/{questionId}/")
+  .onUpdate((snapshot, context) => {
+    try {
+    // @ Retrieve the current and previous value
+    const { questionId } = context.params;
+    const data = snapshot.after.data();
+    const previousData = snapshot.before.data();
+
+    //  ==================================
+      @ Logic if voters were able to unvote
+
+      let {change} = hasChange()
+      if (!change || change.length === 0) return null;
+      if (change.length === 1) {
+        // update notification
+      } else if (change.length > 1) {
+        // loop change
+        // batch update notification
+      }
+    // ===================================
+
+    // We'll only update if the name has changed.
+    // This is crucial to prevent infinite loops.
+
+    if (
+      data.optionOne.votes.length == previousData.optionOne.votes.length &&
+      data.optionTwo.votes.length == previousData.optionTwo.votes.length
+    )
+      return null;
+
+    // ===========================================================
+    // @ Logic if cloud function triggers without event queues
+    // ===========================================================
+    
+    // Retrieve the current voteID
+    let whatChange =
+      data.optionOne.votes.length !== previousData.optionOne.votes.length
+        ? hasChange(data.optionOne.votes, previousData.optionOne.votes)
+        : data.optionTwo.votes.length == !previousData.optionTwo.votes.length
+        ? hasChange(data.optionTwo.votes, previousData.optionTwo.votes)
+        : null;
+
+    if (!whatChange || whatChange === "undefined" || whatChange === null) {
+      return null;
+    }
+
+    if (whatChange.length === 1 && whatChange.includes(context.auth.uid)) {
+      return db.doc(`/notifications/${snapshot.id}`).set({
+        createdAt: new Date().toISOString(),
+        recipient: snapshot.data().authorId,
+        sender: context.auth.uid,
+        type: "vote",
+        read: false,
+        questionId: questionId
+      });
+    } else if (whatChange.length > 1) {
+      return whatChange.forEach(voteID => {
+        const notify = db.doc(`/notifications/${snapshot.id}`);
+        db.batch().create(notify, {
+          createdAt: new Date().toISOString(),
+          recipient: snapshot.data().authorId,
+          sender: voteID,
+          type: "vote",
+          read: false,
+          questionId: questionId
+        });
+        return batch.commit();
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+// Example from firebase documentation
+// Listen for updates to any `user` document.
+exports.countNameChanges = functions.firestore
+    .document('users/{userId}')
+    .onUpdate((change, context) => {
+      // Retrieve the current and previous value
+      const data = change.after.data();
+      const previousData = change.before.data();
+
+      // We'll only update if the name has changed.
+      // This is crucial to prevent infinite loops.
+      if (data.name == previousData.name) return null;
+
+      // Retrieve the current count of name changes
+      let count = data.name_change_count;
+      if (!count) {
+        count = 0;
+      }
+
+      // Then return a promise of a set operation to update the count
+      return change.after.ref.set({
+        name_change_count: count + 1
+      }, {merge: true});
+    });
+
+ */
