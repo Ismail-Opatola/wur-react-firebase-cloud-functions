1daba78804635194b128e2adec96b83687fc2d5b
Ismail Opatola <opatolamails@gmail.com>
2019-06-19 12:19:17 +0100

    trigger 2 cloud-funct  on user_account_delete
    delete_file, on_user_account_delete
    
    both functions clears user data from database and storage bucket

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,10 @@
 const functions = require("firebase-functions"),
   app = require("express")(),
-  { admin, db } = require("./util/admin");
-(FBAuth = require("./util/fbAuth")), (cors = require("cors"));
+  { admin, db, storage } = require("./util/admin"),
+  FieldValue = admin.firestore.FieldValue,
+  FBAuth = require("./util/fbAuth"),
+  cors = require("cors");
+
 app.use(cors({ origin: true }));
 
 const {
@@ -24,10 +27,14 @@ const {
 } = require("./handlers/questions");
 const { getLeaderBoard } = require("./handlers/leaderboard");
 
+// ================
+// EXPRESS ROUTES
+// ================
+
 // users routes
 app.post("/signup", signup);
 app.post("/sessions", login);
-app.delete("/sessions", FB, signOut);
+app.delete("/sessions", FBAuth, signOut);
 app.post("/user/image", FBAuth, uploadImage);
 app.post("/user", FBAuth, addUserDetails);
 app.get("/user", FBAuth, getAuthenticatedUser);
@@ -45,16 +52,101 @@ app.get("/question/:questionId", getQuestion);
 // leaderboard route
 app.get("/leaderboard", getLeaderBoard);
 
-app.get("/", (request, response) => {
-  response.send("Hello from Firebase!");
-});
-
 exports.api = functions.https.onRequest(app);
 
+// =============================================
 // @ FUNCTIONS >> CLOUD FIRESTORE EVENT TRIGGERS
-// @ ref: https://firebase.google.com/docs/functions/firestore-events
-//        https://cloud.google.com/functions/docs/bestpractices/tips#performance
-//        https://www.youtube.com/playlist?list=PLIivdWyY5sqK5zce0-fd1Vam7oPY-s_8X
+// =============================================
+
+/* 
+@ Delete User Account
+ @ delete user Email from firebase-auth
+ @ delete user account 
+ @ delete userImg from storage
+ @ delete user created questions from questions collection
+ onUserAccountDelete triggers onQuestionDelete
+ @ delete user notifications
+ @ delete user created questionID from other users votes and decrement score
+
+ let defaultImage = "no-image"; **GLOBAL_VARIABLE**
+ 'https://firebasestorage.googleapis.com/v0/b/would-you-rather-app-c5895.appspot.com/o/no-img.png?alt=media'; */
+
+// @ Delete User Image from Storage when user delete his/her account
+exports.deleteFile = functions
+  .region("us-central1")
+  .firestore.document("/users/{userId}")
+  .ondelete((change, context) => {
+    let defaultImage = "no-image",
+      imageUrl = change.data().imageUrl,
+      [, , , , , bucket, ...rest] = imageUrl.split("appspot.com"),
+      filename = rest
+        .toString()
+        .replace(",", "/")
+        .split("?alt=media")[0];
+
+    if (filename.includes(defaultImage)) return Promise.resolve();
+    console.log("bucket=", bucket, "filename=", filename);
+
+    // using Firebase-admin SDK
+    return storage
+      .bucket(bucket)
+      .file(filename)
+      .delete()
+      .then(() => {
+        console.log(`gs://${bucket}/${filename} deleted.`);
+      })
+      .catch(err => {
+        console.error("ERROR:", err);
+      });
+  });
+
+// @ Clean all data refrences, when user delete his/her account (notifications, votes, created questions etc...)
+exports.onUserAccountDelete = functions
+  .region("us-central1")
+  .firestore.document("/users/{userId}")
+  .onDelete((change, context) => {
+    // const { questionId } = context.params;
+    const batch = db.batch();
+    return db
+      .collection("questions")
+      .where("authorId", "==", snapshot.id)
+      .get()
+      .then(data => {
+        data.forEach(doc => {
+          // @ delete all question created by user
+          batch.delete(db.doc(`questions/${doc.id}`));
+        });
+        return db
+          .collection("questions")
+          .where("optionOne.votes", "array-contains", snapshot.id)
+          .get();
+      })
+      .then(data => {
+        data.forEach(doc => {
+          // @ remove user vote from all other users questions
+          batch.update(db.doc(`questions/${doc.id}`), {
+            "optionOne.votes": FieldValue.arrayRemove(snapshot.id)
+          });
+        });
+        return db
+          .collection("questions")
+          .where("optionTwo.votes", "array-contains", snapshot.id)
+          .get();
+      })
+      .then(data => {
+        data.forEach(doc => {
+          // @ remove user vote from all other users questions
+          batch.update(db.doc(`questions/${doc.id}`), {
+            "optionTwo.votes": FieldValue.arrayRemove(snapshot.id)
+          });
+        });
+        // @ other users votes and score to this user created questions would be updated by onQuestionDelete trigger, onQuestionDelete trigger becomes active as soon as we start deleting all created questions by user
+        return batch.commit();
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  });
 
 // @ if user profile image has changed
 // @ Update all user's created questions img field
@@ -74,6 +166,9 @@ exports.onUserImageChange = functions
             batch.update(question, { authorImg: change.after.data().imageUrl });
           });
           return batch.commit();
+        })
+        .catch(err => {
+          console.error(err);
         });
     } else return true;
   });
@@ -137,16 +232,16 @@ exports.onQuestionDelete = functions
         data.forEach(doc => {
           const user = db.doc(`users/${doc.id}`);
           batch.update(user, {
-            votes: admin.firestore.FieldValue.arrayRemove(questionId),
-            score: admin.firestore.FieldValue.increment(-1)
+            votes: FieldValue.arrayRemove(questionId),
+            score: FieldValue.increment(-1)
           });
         });
         return db.doc(`users/${snapshot.data().authorId}`);
       })
       .then(data => {
         batch.update(data, {
-          questions: admin.firestore.FieldValue.arrayRemove(questionId),
-          score: admin.firestore.FieldValue.increment(-1)
+          questions: FieldValue.arrayRemove(questionId),
+          score: FieldValue.increment(-1)
         });
         return db
           .collection("notifications")
@@ -162,115 +257,3 @@ exports.onQuestionDelete = functions
       })
       .catch(err => console.error(err));
   });
-
-// UNMARKED
-
-// Global variable
-// function hasChange(votes, previousVotes) {
-//   let change = votes.filter(vote => !previousVotes.includes(vote));
-//   return change;
-// }
-
-/**
- * exports.createNotificationOnVote = functions
-  .region("us-central1")
-  .firestore.document("/questions/{questionId}/")
-  .onUpdate((snapshot, context) => {
-    try {
-    // @ Retrieve the current and previous value
-    const { questionId } = context.params;
-    const data = snapshot.after.data();
-    const previousData = snapshot.before.data();
-
-    //  ==================================
-      @ Logic if voters were able to unvote
-
-      let {change} = hasChange()
-      if (!change || change.length === 0) return null;
-      if (change.length === 1) {
-        // update notification
-      } else if (change.length > 1) {
-        // loop change
-        // batch update notification
-      }
-    // ===================================
-
-    // We'll only update if the name has changed.
-    // This is crucial to prevent infinite loops.
-
-    if (
-      data.optionOne.votes.length == previousData.optionOne.votes.length &&
-      data.optionTwo.votes.length == previousData.optionTwo.votes.length
-    )
-      return null;
-
-    // ===========================================================
-    // @ Logic if cloud function triggers without event queues
-    // ===========================================================
-    
-    // Retrieve the current voteID
-    let whatChange =
-      data.optionOne.votes.length !== previousData.optionOne.votes.length
-        ? hasChange(data.optionOne.votes, previousData.optionOne.votes)
-        : data.optionTwo.votes.length == !previousData.optionTwo.votes.length
-        ? hasChange(data.optionTwo.votes, previousData.optionTwo.votes)
-        : null;
-
-    if (!whatChange || whatChange === "undefined" || whatChange === null) {
-      return null;
-    }
-
-    if (whatChange.length === 1 && whatChange.includes(context.auth.uid)) {
-      return db.doc(`/notifications/${snapshot.id}`).set({
-        createdAt: new Date().toISOString(),
-        recipient: snapshot.data().authorId,
-        sender: context.auth.uid,
-        type: "vote",
-        read: false,
-        questionId: questionId
-      });
-    } else if (whatChange.length > 1) {
-      return whatChange.forEach(voteID => {
-        const notify = db.doc(`/notifications/${snapshot.id}`);
-        db.batch().create(notify, {
-          createdAt: new Date().toISOString(),
-          recipient: snapshot.data().authorId,
-          sender: voteID,
-          type: "vote",
-          read: false,
-          questionId: questionId
-        });
-        return batch.commit();
-      });
-    }
-  } catch (err) {
-    console.error(err);
-  }
-});
-
-// Example from firebase documentation
-// Listen for updates to any `user` document.
-exports.countNameChanges = functions.firestore
-    .document('users/{userId}')
-    .onUpdate((change, context) => {
-      // Retrieve the current and previous value
-      const data = change.after.data();
-      const previousData = change.before.data();
-
-      // We'll only update if the name has changed.
-      // This is crucial to prevent infinite loops.
-      if (data.name == previousData.name) return null;
-
-      // Retrieve the current count of name changes
-      let count = data.name_change_count;
-      if (!count) {
-        count = 0;
-      }
-
-      // Then return a promise of a set operation to update the count
-      return change.after.ref.set({
-        name_change_count: count + 1
-      }, {merge: true});
-    });
-
- */
